9b9ded580014647f47a7e21de206b498f55cd809
Micah Weston <micah.weston@gmail.com>
2021-09-10 14:28:00 -0500

    refactor.

--- app.js
+++ app.js
@@ -1,52 +1,56 @@
-const p1Score = document.querySelector('#p1-score');
-const p2Score = document.querySelector('#p2-score');
-const p1Button = document.querySelector('#p1-add');
-const p2Button = document.querySelector('#p2-add');
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1-add'),
+    display: document.querySelector('#p1-score')
+}
+
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2-add'),
+    display: document.querySelector('#p2-score')
+}
+
 const resetButton  = document.querySelector('#reset');
 const scoreSelect = document.querySelector('#winning');
 
-let p1ScoreKeeper = 0;
-let p2ScoreKeeper = 0;
-let winningScore = 0;
+let winningScore = 3;
 let isGameOver = false;
+
+function updateScores(player, opponent) {
+    if (!isGameOver) { 
+        player.score += 1;   
+        if (player.score === winningScore) {  
+            isGameOver = true;
+            player.display.classList.add('has-text-success-dark')
+            opponent.display.classList.add('has-text-danger-dark')
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        } 
+        player.display.textContent = player.score;
+    }
+}
 
 scoreSelect.addEventListener('change', function () {
     winningScore = parseInt(this.value);
-    reset()
+    reset();
 })
 
-p1Button.addEventListener('click', function () {
-    if (!isGameOver) { 
-        p1ScoreKeeper += 1;   
-        if (p1ScoreKeeper === winningScore) {  
-            isGameOver = true;
-            p1Score.classList.add('has-text-success-dark')
-            p2Score.classList.add('has-text-danger-dark')
-        } 
-        p1Score.textContent = p1ScoreKeeper;
-    }
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2);
 })
 
-p2Button.addEventListener('click', function () {
-    if (!isGameOver) { 
-        p2ScoreKeeper += 1;  
-        if (p2ScoreKeeper === winningScore) {  
-            isGameOver = true;
-            p2Score.classList.add('has-text-success-dark')
-            p1Score.classList.add('has-text-danger-dark')
-        } 
-        p2Score.textContent = p2ScoreKeeper;
-    }
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1);
 })
 
 resetButton.addEventListener('click', reset)
 
 function reset() {
-    p1Score.textContent = 0;
-    p2Score.textContent = 0;
-    p1ScoreKeeper = 0;
-    p2ScoreKeeper = 0;
     isGameOver = false;
-    p1Score.classList.remove('has-text-success-dark', 'has-text-danger-dark');
-    p2Score.classList.remove('has-text-success-dark', 'has-text-danger-dark');
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success-dark', 'has-text-danger-dark')
+        p.button.disabled = false;
+    }
 }
